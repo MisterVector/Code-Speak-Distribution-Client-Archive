dc7ea90ee7f30d96bac916301e9a3dc79cf48619
MisterVector <whizkid3000@hotmail.com>
2020-04-12 11:49:27 -0700

    Added a configuration class

---
+++ src/org/codespeak/distribution/client/Configuration.java
@@ -0,0 +1,11 @@
+package org.codespeak.distribution.client;
+
+/**
+ * A configuration class
+ *
+ * @author Vector
+ */
+public class Configuration {
+    public static final String PROGRAM_VERSION = "0.0.0";
+    public static final String PROGRAM_NAME = "Code Speak Distribution Client v" + PROGRAM_VERSION;
+}
