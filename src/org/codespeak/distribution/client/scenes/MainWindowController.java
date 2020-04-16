dd9376d98c09d9359b07c443f181a9136d02d067
MisterVector <whizkid3000@hotmail.com>
2020-04-15 22:45:36 -0700

    When the main scene loads, query all categories, dependencies and programs and load them

--- src/org/codespeak/distribution/client/scenes/MainWindowController.java
+++ src/org/codespeak/distribution/client/scenes/MainWindowController.java
@@ -10,7 +10,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.codespeak.distribution.client.data.Category;
+import org.codespeak.distribution.client.data.DataManager;
+import org.codespeak.distribution.client.data.Dependency;
+import org.codespeak.distribution.client.data.Program;
+import org.codespeak.distribution.client.data.query.InformationListQueryResponse;
+import org.codespeak.distribution.client.data.query.QueryResponse;
+import org.codespeak.distribution.client.data.query.QueryTypes;
 import org.codespeak.distribution.client.objects.ProgramTableData;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -30,6 +39,36 @@ public class MainWindowController implements Initializable {
         programsTableNameColumn.setCellValueFactory(new PropertyValueFactory<ProgramTableData, String>("name"));
         programsTableVersionColumn.setCellValueFactory(new PropertyValueFactory<ProgramTableData, String>("version"));
         programsTableReleaseTimeColumn.setCellValueFactory(new PropertyValueFactory<ProgramTableData, String>("releaseTime"));
+
+        InformationListQueryResponse response = QueryTypes.getQueryResponse(QueryTypes.GET_CATEGORIES);
+        JSONArray jsonContents = response.getContents();
+
+        for (int i = 0; i < jsonContents.length(); i++) {
+            JSONObject obj = jsonContents.getJSONObject(i);
+            Category category = Category.fromJSON(obj);
+            
+            DataManager.addCategory(category, false);
+        }
+        
+        response = QueryTypes.getQueryResponse(QueryTypes.GET_DEPENDENCIES);
+        jsonContents = response.getContents();
+        
+        for (int i = 0; i < jsonContents.length(); i++) {
+            JSONObject obj = jsonContents.getJSONObject(i);
+            Dependency dependency = Dependency.fromJSON(obj);
+            
+            DataManager.addDependency(dependency, false);
+        }
+
+        response = QueryTypes.getQueryResponse(QueryTypes.GET_PROGRAMS);
+        jsonContents = response.getContents();
+
+        for (int i = 0; i < jsonContents.length(); i++) {
+            JSONObject obj = jsonContents.getJSONObject(i);
+            Program program = Program.fromJSON(obj, false);
+            
+            DataManager.addProgram(program, false);
+        }
     }    
     
 }
