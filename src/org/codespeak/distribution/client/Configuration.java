e59e81562d84329164d81238307a2b2129a1d699
MisterVector <whizkid3000@hotmail.com>
2020-04-19 12:23:41 -0700

    Capitalize the programs folder

--- src/org/codespeak/distribution/client/Configuration.java
+++ src/org/codespeak/distribution/client/Configuration.java
@@ -10,6 +10,6 @@ public class Configuration {
     public static final String PROGRAM_NAME = "Code Speak Distribution Client v" + PROGRAM_VERSION;
     public static final String DISTRIBUTION_URL = "http://distribution.codespeak.org";
     public static final String BACKEND_URL = DISTRIBUTION_URL + "/data_handler.php";
-    public static final String PROGRAMS_FOLDER = "programs";
+    public static final String PROGRAMS_FOLDER = "Programs";
     public static final String STORED_PROGRAMS_FILE = "programs.json";
 }
