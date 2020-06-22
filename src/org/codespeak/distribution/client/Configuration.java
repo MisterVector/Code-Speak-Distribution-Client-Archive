054ea857f52a55bfa8da22b54e5cc2d482835f71
MisterVector <whizkid3000@hotmail.com>
2020-06-22 07:33:57 -0700

    Both domains are now using HTTPS

--- src/org/codespeak/distribution/client/Configuration.java
+++ src/org/codespeak/distribution/client/Configuration.java
@@ -17,9 +17,9 @@ public class Configuration {
     public static final String PROGRAM_VERSION = "0.0.0";
     public static final String PROGRAM_NAME = "Code Speak Distribution Client";
     public static final String PROGRAM_TITLE = PROGRAM_NAME + " v" + PROGRAM_VERSION;
-    public static final String WEBSITE_URL = "http://www.codespeak.org";
+    public static final String WEBSITE_URL = "https://www.codespeak.org";
     public static final String CONTACT_ME_PAGE = WEBSITE_URL + "/contact";
-    public static final String DISTRIBUTION_URL = "http://distribution.codespeak.org";
+    public static final String DISTRIBUTION_URL = "https://distribution.codespeak.org";
     public static final String BACKEND_URL = DISTRIBUTION_URL + "/data_handler.php";
     public static final String PROGRAMS_FOLDER = "programs";
     public static final String LOGS_FOLDER = "logs";
