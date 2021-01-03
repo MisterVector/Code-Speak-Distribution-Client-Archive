89d084173f6b348094002eeba24ce651224a33ba
MisterVector <whizkid3000@hotmail.com>
2021-01-03 12:04:44 -0800

    Removed unused imports

--- src/org/codespeak/distribution/client/scenes/ProgramDependenciesSceneController.java
+++ src/org/codespeak/distribution/client/scenes/ProgramDependenciesSceneController.java
@@ -8,7 +8,6 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
